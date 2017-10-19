64e7c6d6a76673c72029fcc24f27cac91a8b7253
Michal <m14lickiewicz@gmail.com>
2017-10-19 20:48:39 +0200

    Install material angular.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from '@angular/core';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent],
+    FooterComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
   exports: [
     HeaderComponent,
     FooterComponent
